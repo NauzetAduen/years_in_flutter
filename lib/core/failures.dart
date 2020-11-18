bc5eb6f413c710a44bc9b071dadfc80d9249e4ac
Nauzet Hernández <nauzet.aduen@gmail.com>
2020-11-18 23:25:57 +0000

    ADD basic database failure and exception for flowcontrol

--- lib/core/failures.dart
+++ lib/core/failures.dart
@@ -1,3 +1,8 @@
 import 'package:equatable/equatable.dart';
 
-abstract class Failure extends Equatable {}
+abstract class Failure extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+class DatabaseFailure extends Failure {}
