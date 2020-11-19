cc667d3c65acbd957de5fde6fba77af1e4d98795
Nauzet Hernández <nauzet.aduen@gmail.com>
2020-11-19 23:01:44 +0000

    ADD connectivity functionality and tests

--- lib/core/network/network_info.dart
+++ lib/core/network/network_info.dart
@@ -1,3 +1,15 @@
+import 'package:connectivity/connectivity.dart';
+
 abstract class NetworkInfo {
   Future<bool> get isConnected;
+}
+
+class NetworkInfoImpl implements NetworkInfo {
+  final Connectivity connectivity;
+
+  NetworkInfoImpl(this.connectivity);
+  @override
+  Future<bool> get isConnected async {
+    return ConnectivityResult.none != await connectivity.checkConnectivity();
+  }
 }
