4c69346d8beedceed351adca3bcf51f7418665c6
Nauzet Hernández <nauzet.aduen@gmail.com>
2020-11-20 04:27:14 +0000

    ADD basic blocBuilder in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:page_transition/page_transition.dart';
 
+import 'cubit/pixelsinyears_cubit.dart';
+import 'injector.dart' as injector;
+
 void main() {
+  injector.init();
   runApp(MyApp());
 }
 
@@ -35,8 +40,25 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("maybe not centered"),
+        child: BlocProvider(
+          create: (context) => injector.sl<PixelsinyearsCubit>(),
+          child: BlocBuilder<PixelsinyearsCubit, PixelsinyearsState>(
+              builder: (context, state) {
+            if (state is PixelsinyearsInitial) {
+            } else if (state is PixelsinyearsLoading) {
+            } else if (state is PixelsinyearsLoadedEmpty) {
+            } else if (state is PixelsinyearsLoaded) {
+            } else {
+              //state is PixelsinyearsError
+            }
+          }),
+        ),
       ),
     );
   }
 }
+
+//TODO
+//Commits on Mar 11, 2020
+//resocoder video cubits : https://www.youtube.com/watch?v=y564ETOCog8
+// cubits 12:55 -> 35:55
